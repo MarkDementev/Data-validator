8e8544c15f15118de9243d60fafc1cf12fa5aec1
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-03-13 15:06:34 +0300

    add validator method map

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.schemas.StringSchema;
 import hexlet.code.schemas.NumberSchema;
+import hexlet.code.schemas.MapSchema;
 
 public class Validator {
     public StringSchema string() {
@@ -10,5 +11,9 @@ public class Validator {
 
     public NumberSchema number() {
         return new NumberSchema();
+    }
+
+    public MapSchema map() {
+        return new MapSchema();
     }
 }
