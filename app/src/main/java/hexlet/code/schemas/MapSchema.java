5ebb56c500c3dcc3077d8f78dc77b377e53478f9
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-04-18 14:08:15 +0300

    try to pass Hexlet tests - refactor shape validation

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -9,7 +9,9 @@ import java.util.function.Predicate;
 public final class MapSchema extends BaseSchema {
     private static final String ONLY_MAPS_CHECK_NAME = "onlyMapsCheck";
     private static final String SIZE_OF_CHECK_NAME = "sizeofCheck";
+    private static final String SHAPE_CHECK_NAME = "shapeCheck";
     Map<String, BaseSchema> schemas = new HashMap<>();
+//    Map<String, BaseSchema> schemas = new HashMap<>();
 
     public MapSchema() {
         addCheck(ONLY_MAPS_CHECK_NAME, onlyMapsCheck());
@@ -22,26 +24,34 @@ public final class MapSchema extends BaseSchema {
     }
 
     public MapSchema(boolean isRequired, Map<String, Predicate<Object>> checks,
+                     String checkToAddName, Predicate<Object> checkToAdd,
                      Map<String, BaseSchema> schemas) {
         super(isRequired, checks);
         this.schemas = schemas;
+        addCheck(checkToAddName, checkToAdd);
     }
 
-    public boolean isValid(Map<String, Object> mapToCheck) {
-        if (mapToCheck == null) {
-            return !isRequired;
-        }
+//    public MapSchema(boolean isRequired, Map<String, Predicate<Object>> checks,
+//                     Map<String, BaseSchema> schemas) {
+//        super(isRequired, checks);
+//        this.schemas = schemas;
+//    }
 
-        for (Map.Entry<String, BaseSchema> schema : schemas.entrySet()) {
-            String schemaKey = schema.getKey();
-            Object mapToCheckValue = mapToCheck.get(schemaKey);
-
-            if (!schema.getValue().isValid(mapToCheckValue)) {
-                return false;
-            }
-        }
-        return true;
-    }
+//    public boolean isValid(Map<String, Object> mapToCheck) {
+//        if (mapToCheck == null) {
+//            return !isRequired;
+//        }
+//
+//        for (Map.Entry<String, BaseSchema> schema : schemas.entrySet()) {
+//            String schemaKey = schema.getKey();
+//            Object mapToCheckValue = mapToCheck.get(schemaKey);
+//
+//            if (!schema.getValue().isValid(mapToCheckValue)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
     public MapSchema required() {
         setIsRequiredTrue();
@@ -56,8 +66,15 @@ public final class MapSchema extends BaseSchema {
 
     public MapSchema shape(Map<String, BaseSchema> newSchemas) {
         this.schemas = newSchemas;
-        return new MapSchema(isRequired, checks, schemas);
+        return new MapSchema(isRequired, checks,
+                SHAPE_CHECK_NAME, shapeCheck(newSchemas),
+                schemas);
     }
+
+//    public MapSchema shape(Map<String, BaseSchema> newSchemas) {
+//        this.schemas = newSchemas;
+//        return new MapSchema(isRequired, checks, schemas);
+//    }
 
     private Predicate<Object> onlyMapsCheck() {
         return p -> (p instanceof Map);
@@ -68,6 +85,24 @@ public final class MapSchema extends BaseSchema {
             ObjectMapper mapper = new ObjectMapper();
             Map<?, ?> pMap = mapper.convertValue(p, Map.class);
             return pMap.size() == neededSize;
+        };
+    }
+
+    private Predicate<Object> shapeCheck(Map<String, BaseSchema> inputSchemas) {
+        //p - видимо, это вводимая снаружи Map<String, Object> mapToCheck
+        return p -> {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<?, ?> pMap = mapper.convertValue(p, Map.class);
+
+            for (Map.Entry<String, BaseSchema> schema : inputSchemas.entrySet()) {
+                String schemaKey = schema.getKey();
+                Object mapToCheckValue = pMap.get(schemaKey);
+
+                if (!schema.getValue().isValid(mapToCheckValue)) {
+                    return false;
+                }
+            }
+            return true;
         };
     }
 }
